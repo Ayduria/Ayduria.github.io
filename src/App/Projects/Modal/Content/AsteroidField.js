e1ef21f756a42b392a2080192773b0ea90e9602c
Ann-Laurie <lapointeannlaurie@gmail.com>
2022-11-02 00:58:30 -0400

    Add AsteroidField description

--- src/App/Projects/Modal/Content/AsteroidField.js
+++ src/App/Projects/Modal/Content/AsteroidField.js
@@ -7,16 +7,55 @@ function AsteroidField( {} ) {
         <div className="project-page">
             <div className="project-section">
                 <h1>Description du projet</h1>
+                <p>Réalisé dans le cadre d'un exercice de cours, Asteroid Field, sous son nom non-officiel, est un micro-jeu dans lequel le joueur contrôle un vaisseau spatial dans l'espace.</p>
+
+                <p>Le vaisseau se déplace au travers un immense champ de milliers d'astéroïdes en mouvement et doit tirer sur ces derniers pour les détruire.</p>
                 
             </div>
 
             <div className="project-section">
                 <h1>Mon implication</h1>
+
+                <h2>Instanciateur d'objets</h2>
+                <p>J'ai implémenté une fenêtre d'inspecteur personnalisé dans l'engine permettant d'instancier dans la scène un certain nombre d'un objet, à la racine ou enfants d'un parent donné.</p>
+
+                <img
+                    alt="Object spawner window"
+                    className="w-24 mb-5"
+                    src="./img/projects/object-spawner-window.jpg"
+                />
+
+                <p>Autre que le nombre d'objets, cette fenêtre permet également de modifier d'autres paramètres de spawn dont le préfixe des objets instanciés, la distance maximale à laquelle ils peuvent être instanciés et un intervalle déterminant leur taille minimale et maximale.</p>
+
+                <p>On peut également consulter le nombre d'objects ayant déjà été instanciés et réinitialiser ce nombre, supprimant tous les objets déjà instanciés dans la scène.</p>
+
+                <p>C'est via cette fenêtre que j'ai instancié les 5000 astéroïdes se trouvant dans le jeu.</p>
+
+                <h2>Implémentation du Job System</h2>
+                <p>Une partie importante de cet exercice était de nous familiariser avec le Unity Job System. Le jeu comportant 5000 astéroïdes se déplaçant indépendamment les uns des autres, cela était essentiel pour conserver une performance acceptable.</p>
+
+                <img
+                    alt="Asteroids moving"
+                    className="w-24 mb-5"
+                    src="./img/projects/asteroids-rising.gif"
+                />
+
+                <p>Le mouvement des astéroïdes est donc effectué sur deux threads différents; l'un se charge des calculs de vélocité, accélération et rotation de chaque astéroïde, alors que l'autre se charge de mettre à jour la position des astéroïdes en leur appliquant des transformations.</p>
+
+                <img
+                    alt="Asteroids moving"
+                    className="w-24 mb-5"
+                    src="./img/projects/asteroid-jobs.jpg"
+                />
+
+                <p>Ainsi, les calculs et transformations se produisant simultanément sur deux threads différents, 5000 astéroïdes se déplacent à l'écran en même temps de façon non-uniforme et le jeu reste tout de même fluide.</p>
                 
             </div>
 
             <div className="project-section">
                 <h1>Défis de conception</h1>
+                <h2>Performance</h2>
+                <p>Conserver une bonne framerate n'a pas été facile durant cet exercice. En plus de l'implémentation de multithreading, j'ai dû optimiser au maximum les modèles et textures des astéroïdes afin que ces derniers affectent le moins possible la performance.</p>
             
             </div>
 
