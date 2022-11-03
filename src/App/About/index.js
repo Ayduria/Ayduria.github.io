10e2c7f0bbe68f703132c1a76ed449300522e9cf
Ann-Laurie <lapointeannlaurie@gmail.com>
2022-11-02 22:05:59 -0400

    Added CV

--- src/App/About/index.js
+++ src/App/About/index.js
@@ -17,7 +17,7 @@ function About() {
                     <p className="mb-8 leading-relaxed">Je suis présentement dans la dernière année de mon <b>baccalauréat en développement de jeux vidéo</b> à l'UQAC. Avant d'entrer à l'université, j'ai complété mes études collégiales dans le domaine du web en Techniques d'intégration multimédia au Cégep de Jonquière, où j'ai développé un intérêt marqué pour la programmation.</p>
                     <p className="mb-10">Étant une <b>grande passionnée de jeux vidéo</b> depuis aussi longtemps que je me souvienne, la décision de poursuivre mes études dans un domaine qui combine ainsi mes intérêts ne fut pas bien difficile.</p>
                     <a
-                        href="./files/CV-placeholder.pdf"
+                        href="./files/Portfolio-CV.pdf"
                         target="_blank"
                         rel="noopener noreferrer"
                         id="cv-btn"
