1612f46e657e138e295e843eb89c82dbd478f851
Ann-Laurie <lapointeannlaurie@gmail.com>
2023-12-17 12:22:15 -0500

    Update copyright name

--- src/App/Copyright/Copyright_EN/index.js
+++ src/App/Copyright/Copyright_EN/index.js
@@ -5,7 +5,7 @@ import React from "react"
 function Copyright() {
     return(
         <footer className="text-gray-400 bg-gray-800 py-5">
-          <b>© Ann-Laurie Lapointe, 2023</b> | Hosting via <a href="https://pages.github.com/" target="_blank" rel="noopener noreferrer">Github Pages</a> | Icons from <a href="https://heroicons.com/" target="_blank" rel="noopener noreferrer">Heroicons</a> and <a href="https://www.flaticon.com/" target="_blank" rel="noopener noreferrer">Flaticon</a>
+          <b>© Eli Lapointe, 2023</b> | Hosting via <a href="https://pages.github.com/" target="_blank" rel="noopener noreferrer">Github Pages</a> | Icons from <a href="https://heroicons.com/" target="_blank" rel="noopener noreferrer">Heroicons</a> and <a href="https://www.flaticon.com/" target="_blank" rel="noopener noreferrer">Flaticon</a>
         </footer>
     );
 }
